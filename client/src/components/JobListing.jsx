ad993a48df169a86993eb845085d56c1d4ba1209
shreesha-codes <basaravanishreesha@gmail.com>
2025-12-24 16:11:55 +0530

    feat: add toggle functionality for filter visibility in JobListing component

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useState,useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations } from '../assets/assets'
 import JobCard from './JobCard'
 
 const JobListing = () => {
     const { searchFilter, isSearched, setSearchfilter, jobs } = useContext(AppContext)
+    const [showFilter, setShowFilter] = useState(true)
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* side bar*/}
@@ -30,8 +31,12 @@ const JobListing = () => {
           </>
         )}
 
+        <button onClick={() => setShowFilter(!showFilter)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+            {showFilter ? "Close": "Filters"}
+        </button>
+
         {/*Category Filter*/} 
-      <div className='max-lg:hidden'>
+      <div className={showFilter ? "":"max-lg:hidden"}>
         <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
         <ul className='space-y-4 text-gray-600'>
             {JobCategories.map((category, index) => (
@@ -43,7 +48,7 @@ const JobListing = () => {
         </ul>
       </div>
       {/*Search by location */}
-      <div className='max-lg:hidden mt-14'>
+      <div className={showFilter ? "":"max-lg:hidden"}>
         <h4 className='font-medium text-lg py-4'>Search by Locations</h4>
         <ul className='space-y-4 text-gray-600'>
             {JobLocations.map((location, index) => (

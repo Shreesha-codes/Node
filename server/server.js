7130a7825ef293eb4d5747326e0d2faf359f53b6
shreesha-codes <basaravanishreesha@gmail.com>
2025-12-25 14:46:36 +0530

    feat: update package.json and server.js to use ES modules and add new dependencies

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('Api working');
+});
+
+//port
+const PORT = process.env.PORT || 5000;
+
+// Start server 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
